d8d2a3101c2d220f40ed658b1d5e59b48ac48be5
joydeep-git <joydeep.accounts@tuta.io>
2025-04-06 06:58:32 +0530

    random name generator function created

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -1,15 +1,16 @@
 
 
-// third party
 import { adjectives, starWars, uniqueNamesGenerator } from "unique-names-generator";
 
 
-const randomNameGenerator = (): string => {
+
+// #### Generate random name
+export const randomNameGenerator = (): string => {
+
   return uniqueNamesGenerator({
     dictionaries: [adjectives, starWars],
     separator: " ",
     length: 2
-  })
-}
+  });
 
-export default randomNameGenerator;
+}
