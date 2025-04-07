e37c2740e5bf6d39c552b1c1a314f58ceeb6c90d
joydeep-git <joydeep.accounts@tuta.io>
2025-04-07 18:17:29 +0530

    token validator completed

---
+++ src/middleware/tokenValidatorMiddleware.ts
@@ -0,0 +1,70 @@
+
+
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+import { StatusCode, UserDataType } from "../types";
+import authModel from "../models/authModel";
+import { errRes } from "../utils/helperFunctions";
+
+
+
+const tokenValidator = async (req: Request, res: Response, next: NextFunction) => {
+
+  // ### get user's id from params
+  const { id } = req.params;
+
+
+  // #### if id is not present
+  if (!id) {
+    return next(errRes("No ID found! Please provide USER ID", StatusCode.BAD_REQUEST));
+  }
+
+
+
+  // ##### get token
+  const token: string = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+
+  // #### if token isnt there
+  if (!token) {
+    return next(errRes("No TOKEN found!", StatusCode.UNAUTHORIZED));
+  }
+
+
+  // #### if token is present then get userId
+  const { userId } = jwt.verify(token, process.env.JWT_SECRET_KEY!) as JwtPayload;
+
+
+  // #### invalid token check
+  if (!userId) {
+    return next(errRes("Invalid token!", StatusCode.UNAUTHORIZED));
+  }
+
+
+  // #### GET user details from userId
+  const val: UserDataType = await authModel.findUserById(userId);
+
+
+  if (!val) {
+    return next(errRes("User not found!", StatusCode.UNAUTHORIZED));
+  }
+
+
+
+  // #### compare id with userId given by user
+
+  if (val.id !== id) {
+
+    return next(errRes("Unauthorized access!", StatusCode.UNAUTHORIZED));
+
+  } else {
+
+    req.user = val;
+
+    next();
+  }
+
+}
+
+export default tokenValidator;
