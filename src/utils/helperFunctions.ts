454238c7d06e8ae9daa11bae2c045a23a685c7e2
joydeep-git <joydeep.accounts@tuta.io>
2025-04-07 18:22:19 +0530

    Helper functions - 1. generate random names 2. a function calling ErrorHandler to revent writing same code every time 3. Valid email checker

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -1,6 +1,7 @@
 
 
 import { adjectives, starWars, uniqueNamesGenerator } from "unique-names-generator";
+import ErrorHandler from "../errorHandlers/errorHandler";
 
 
 
@@ -12,5 +13,21 @@ export const randomNameGenerator = (): string => {
     separator: " ",
     length: 2
   });
+
+}
+
+
+// error response reusable function
+export const errRes = (message: string, status: number): ErrorHandler => {
+  return new ErrorHandler({ message, status });
+}
+
+
+// test email
+export const isValidEmail = (email: string): boolean => {
+  
+  const re: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+  return re.test(email);
 
 }
