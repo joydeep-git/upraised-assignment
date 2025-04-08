1a940fbb840914d9fa0d1a46d706e4ae4c30a7bf
joydeep-git <joydeep.accounts@tuta.io>
2025-04-08 20:18:29 +0530

    refined authRouter

--- src/routers/authRouters.ts
+++ src/routers/authRouters.ts
@@ -5,26 +5,26 @@ import tokenValidator from "../middleware/tokenValidatorMiddleware";
 
 class AuthRouter {
 
-    public router: Router = express.Router();
+  public router: Router = express.Router();
 
-    constructor() {
-
-
-        this.router.post("/sign-in", authController.signIn );
-
-        
-        this.router.post("/sign-up", authController.signUp );
+  constructor() {
 
 
-        this.router.get("/sign-out", tokenValidator, );
+    this.router.post("/sign-in", authController.signIn);
 
 
-        this.router.patch("/update-user", tokenValidator, authController.updateUser );
+    this.router.post("/sign-up", authController.signUp);
 
 
-        this.router.delete("/delete-user", tokenValidator, authController.deleteUser );
+    this.router.delete("/sign-out/:id", tokenValidator, authController.signOut);
 
-    }
+
+    this.router.patch("/update-user/:id", tokenValidator, authController.updateUser);
+
+
+    this.router.delete("/delete-user/:id", tokenValidator, authController.deleteUser);
+
+  }
 
 }
 
